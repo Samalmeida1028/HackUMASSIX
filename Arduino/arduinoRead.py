38f45a597f71360d9b41686f515731aae7a18fd7
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-11-06 12:33:04 -0400

    Added variable threshold

--- Arduino/arduinoRead.py
+++ Arduino/arduinoRead.py
@@ -4,12 +4,13 @@ import time
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1000)      # initialize Arduino
 
 def loop():
+    threshold = initialize()
     intervals = []
     prevPD = 0
     prevPS = 0
     while True:
         voltage = arduino.readline()                # read from the serial line
-        strings = voltage.decode("utf-8").split("|")
+        strings = voltage.decode("utf-8").split(" ")
         vPD = float(strings[0])
         vPS = float(strings[1])
 
@@ -18,7 +19,7 @@ def loop():
         else:
             PS = 0
 
-        if vPD >= 50.00:
+        if vPD >= threshold:
             PD = 1
         else:
             PD = 0
@@ -29,6 +30,16 @@ def loop():
         elif prevPD == 1:
             if PD == 0:
                 intervals.append(time.process_time() - intervalStart)
+
+
+def initialize():
+    refVal = 10
+    for i in range(0,refVal):
+        voltage = arduino.readline()  # read from the serial line
+        strings = voltage.decode("utf-8").split(" ")
+        averageNoise += float(strings[1])
+    averageNoise /= refVal
+    return averageNoise
 
 
 if __name__ == '__main__':
