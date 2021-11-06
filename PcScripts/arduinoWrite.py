48bab708531d51b5ad5326d8f20663cec4d689d6
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-11-06 17:03:51 -0400

    mergining final

--- PcScripts/arduinoWrite.py
+++ PcScripts/arduinoWrite.py
@@ -1,13 +1,18 @@
 import TextToOutput as t
 import serial
+import time
+from struct import *
+arduino = serial.Serial(port='COM9', baudrate=115200, timeout=.1)
+def write_read(x):
+    print(arduino.write(x[0]),
+    arduino.write(x[1]))
+    data = arduino.readline()
+    return data
 
-arduino = serial.Serial(port='COM9', baudrate=9600, timeout=1000)
-
-
-
-string = input('Put string: ')
-
+string = input("Enter a string: ") # Taking input from user
 l = t.textToOutput(string)
+print(l)
 for i in l:
-    tup = i
-    arduino.write(tup)
+    value = write_read(i)
+print(value) # printing the value
+
