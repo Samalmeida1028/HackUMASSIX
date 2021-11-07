3b010cebf73395cca790acc52aa092c62c677e09
bhagattirth <76080774+bhagattirth@users.noreply.github.com>
2021-11-06 23:47:09 -0400

    CHinguun destroyed my demo so I decide to restore it

--- PcScripts/demo.py
+++ PcScripts/demo.py
@@ -4,16 +4,22 @@ import arduinoWrite as aw
 
 
 
-userInput= input("ENTER A SENTENCE TO CONVERT/SEND");
-transformedUserInput= m.TextToOutput(userInput);
+while(True):
+    recieve_= input("Want to Send or Convert Sentence? Y/N: ")
+    while recieve_ != Y  or recieve_ !=N:
+        recieve_= input("INVAILD INPUT. TRY AGAIN Y/N: ")
+
+
+    userInput= input("ENTER A SENTENCE TO CONVERT/SEND");
+    transformedUserInput= m.TextToOutput(userInput);
 
 
 
 
 
 
-continue_= input("Continue? Y/N: ")
-while continue_ != Y  or continue_ !=N:
     continue_= input("Continue? Y/N: ")
+    while continue_ != Y  or continue_ !=N:
+        continue_= input("Continue? Y/N: ")
     if continue_ == 'N':
         break
