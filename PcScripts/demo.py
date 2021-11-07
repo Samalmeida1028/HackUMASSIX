15cb991e74dfeaf96a97c9477f6f1c478026166e
bhagattirth <76080774+bhagattirth@users.noreply.github.com>
2021-11-06 22:09:16 -0400

    Updating the driver

--- PcScripts/demo.py
+++ PcScripts/demo.py
@@ -3,17 +3,23 @@ import arduinoRead as ar
 import arduinoWrite as aw
 
 while(True):
-    recieve_= input("Want to Send or Convert Sentence? Y/N: ")
+    recieve_= input("Rather recieve morse code than send? Y/N: ")
     while recieve_ != Y  or recieve_ !=N:
         recieve_= input("INVAILD INPUT. TRY AGAIN Y/N: ")
 
+    if(recieve_=='Y'):
+        print('hi')
 
-    userInput= input("ENTER A SENTENCE TO CONVERT/SEND");
-    transformedUserInput= m.TextToOutput(userInput);
+    else:
+        keyboardOrSensor=input("Want to type sentence rather than use morse code? Y/N: "):
+        while keyboardOrSensor!= Y  or keyboardOrSensor !=N:
+            keyboardOrSensor= input("INVAILD INPUT. TRY AGAIN Y/N: ")
+        if(keyboardOrSensor=='N'):
+            userInput= input("ENTER A SENTENCE TO CONVERT/SEND");
+            transformedUserInput= m.TextToOutput(userInput);
 
-
-
-
+        else:    
+            print("Morse Code!")
 
 
     continue_= input("Continue? Y/N: ")
