7e6ab5944df365fdee1f92f7dff1a3479e4a0228
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-11-07 00:15:19 -0400

    added String and function

--- PcScripts/arduinoWrite.py
+++ PcScripts/arduinoWrite.py
@@ -2,17 +2,35 @@ import serial
 from PcScripts import TextToOutput as t
 import time
 
-
-def writeToArduino(string):
-    arduino = serial.Serial(port='COM9', baudrate=115200, timeout=1)
-    l = string
-    time.sleep(0.75)
+def config(arduino):
+    time.sleep(1)
+    l = '3131313131666666666'
     for i in l:
         send = str(i)
-        delay = 0.1
+        delay = 1
 
         if send == "2" or send == "3":                      # choose the delay value
-            delay = .6
+            delay = 1
+        elif send != "1":                                   # can only be 6 or 7 if not 2 or 3
+            delay = 3
+
+        arduino.write(bytes(send, 'utf-8'))
+        data = arduino.readline().decode('utf-8')
+
+        print(str(time.ctime()) + ' SENT: {}'.format(send))
+        print(str(time.ctime()) + ' RCVD: {}'.format(data))
+        time.sleep(delay * 0.25)
+
+def writeToArduino(string):
+    arduino = serial.Serial(port='COM9', baudrate=115200, timeout=2)
+    l = string
+    config(arduino)
+    for i in l:
+        send = str(i)
+        delay = 1
+
+        if send == "2" or send == "3":                      # choose the delay value
+            delay = 1
         elif send != "1":                                   # can only be 6 or 7 if not 2 or 3
             delay = 3
 
