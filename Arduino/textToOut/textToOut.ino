712aede06fa2e6dd28bb68bbfecc018084e129de
PieDude2612 <arjun.viswanathan2612@gmail.com>
2021-11-06 22:41:25 -0400

    FINALLY WORKS

--- Arduino/textToOut/textToOut.ino
+++ Arduino/textToOut/textToOut.ino
@@ -1,37 +1,29 @@
-#include "SerialTransfer.h"
-
-SerialTransfer myTransfer;
-
 void setup() {
   Serial.begin(115200);
-  myTransfer.begin(Serial);
   pinMode(7, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(7, HIGH);
-  for (uint16_t i = 0; i < 200; i++) {
-    myTransfer.packet.txBuff[i] = myTransfer.packet.rxBuff[i];
+  if (Serial.available()) {
+    String input_data = Serial.readString();
+    int data[2];
+
+    for (int i = 0; i < input_data.length(); i++) {
+      data[i] = input_data[i];
+    }
+    Serial.println(input_data);
+    bool low_high;
+
+    if (input_data[1] == 0) {
+      low_high = false;
+    } else {
+      low_high = true;
+    }
+
+    if (low_high) {
+      digitalWrite(7, HIGH);
+    } else {
+      digitalWrite(7, LOW);
+    }
   }
-
-  myTransfer.sendData(myTransfer.bytesRead);
-
-  int input_data[2];
-  input_data[0] = myTransfer.packet.txBuff[0];
-  input_data[1] = myTransfer.packet.txBuff[1];
-
-  bool low_high;
-
-  if (input_data[1] == 0) {
-    low_high = false;
-  } else {
-    low_high = true;
-  }
-
-  if (low_high) {
-    digitalWrite(7, HIGH);
-  } else {
-    digitalWrite(7, LOW);
-  }
-  delay(input_data[0] * 1000);
 }
--- PcScripts/arduinoWrite.py
+++ PcScripts/arduinoWrite.py
@@ -1,62 +1,17 @@
-import time
-from pySerialTransfer import pySerialTransfer as txfer
+import serial
 from PcScripts import TextToOutput as t
 
-if __name__ == '__main__':
-    try:
-        link = txfer.SerialTransfer('COM4')
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=1)
 
-        link.open()
-        time.sleep(2)  # allow some time for the Arduino to completely reset
-        send_size = 0
+string = input("Enter a string: ")
+l = t.textToOutput(string)
+print(l)
+i = 0
+while i < len(l) - 2:
+    send = str(l[i]) + str(l[i+1])
+    arduino.write(bytes(send, 'utf-8'))
+    i += 2
+    data = arduino.readline().decode('utf-8')
 
-        ###################################################################
-        # Send a list
-        ###################################################################
-        string = input("Enter a string: ")
-        l = t.textToOutput(string)
-        print(l)
-        list_size = link.tx_obj(l)
-        send_size += list_size
-        link.send(send_size)
-
-        ###################################################################
-        # Wait for a response and report any errors while receiving packets
-        ###################################################################
-        while not link.available():
-            if link.status < 0:
-                if link.status == txfer.CRC_ERROR:
-                    print('ERROR: CRC_ERROR')
-                elif link.status == txfer.PAYLOAD_ERROR:
-                    print('ERROR: PAYLOAD_ERROR')
-                elif link.status == txfer.STOP_BYTE_ERROR:
-                    print('ERROR: STOP_BYTE_ERROR')
-                else:
-                    print('ERROR: {}'.format(link.status))
-
-        # ###################################################################
-        # Parse response list
-        # ###################################################################
-        rec_list_ = link.rx_obj(obj_type=type(l),
-                                obj_byte_size=list_size,
-                                list_format='i')
-
-        print('SENT: {}'.format(l))
-        print('RCVD: {}'.format(rec_list_))
-        print(' ')
-
-    except KeyboardInterrupt:
-        try:
-            link.close()
-        except:
-            pass
-
-    except:
-        import traceback
-
-        traceback.print_exc()
-
-        try:
-            link.close()
-        except:
-            pass
+    print('SENT: {}'.format(send))
+    print('RCVD: {}'.format(data))
