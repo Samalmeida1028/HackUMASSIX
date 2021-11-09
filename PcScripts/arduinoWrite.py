92a88f2f3bddd41383d80e95b66815537c0935a4
PieDude2612 <arjun.viswanathan2612@gmail.com>
2021-11-09 14:35:15 -0500

    Made the code more robust

--- PcScripts/arduinoWrite.py
+++ PcScripts/arduinoWrite.py
@@ -2,6 +2,8 @@ import serial
 import TextToOutput as t
 import time
 
+'''Configure the arduino by sending 4 dit flashes between the computers. This will output a checkpoint
+which should be about 1.25 seconds if everything is configured right given the delays are not changed.'''
 def config(arduino):
     time.sleep(1)
     l = '313131313166'
@@ -14,16 +16,21 @@ def config(arduino):
             delay = 5
 
         arduino.write(bytes(send, 'utf-8'))
-        data = arduino.readline().decode('utf-8')           # return value from arduino
+
+        # data = arduino.readline().decode('utf-8')           # return value from arduino for debugging
+        # print("SENT: {}".format(send))
+        # print("RCVD: {}".format(data))
+
         time.sleep(delay * 0.25)
 
-def writeToArduino(string):
-    arduino = serial.Serial(port='COM3', baudrate=115200, timeout=2)
+'''Takes the characters from the string and flashes the LED in MORSE. Each string is sent into the board
+and the board flashes the LED. A readline() function is used to check if every char is sent properly.'''
+def writeToArduino(string, arduino):
     count = 0
-    l = string
-    instructionsTotal = len(l)
+    instructionsTotal = len(string)                    # the string contains the entire set of instrs
     config(arduino)
-    for i in l:
+
+    for i in string:
         count+=1
         send = str(i)
         print(str(time.ctime())+ " sending %s of %s, %s%% of the way done" % (
@@ -44,9 +51,13 @@ def writeToArduino(string):
         # LED kill sequence after code executes
 
 if __name__ == '__main__':
-    string = input('Enter input String: ')
-    l = t.textToOutput(string)
-    for char in l:
-        if char != '1':
-            print(char, end = '')
-    writeToArduino(l)
+    arduino = serial.Serial(port='COM3', baudrate=115200, timeout=2)  # initialize arduino
+
+    string = input('Enter input string in English: ')
+    l = t.textToOutput(string)                              # get the string of sums to work with
+
+    # for char in l:
+    #     if char != '1':
+    #         print(char, end = '')
+
+    writeToArduino(l, arduino)
--- PcScripts/demo.py
+++ PcScripts/demo.py
@@ -3,21 +3,31 @@ import TextToOutput as T
 import arduinoRead as ar
 import arduinoWrite as aw
 
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1000)
+
 receive = True
 firstReceive = True
 
-toSend = "ALEXANDER GRAHAM BELL"
+# toSend = "ALEXANDER GRAHAM BELL"
 ditval = 1.4
-if receive:
-    arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1000) 
+
+if receive:     # this computer is the receiver
     if firstReceive:
         ditVal = ar.calibrate(1, arduino)
+
     print("CHECKPOINT %.4f" % ditVal)
     print(ar.takeMachineInput(ditVal, arduino))
+
     calibration1 = ar.loop(1, 0.75, 0.3)
     ditVal = ar.calculateOddAverage(calibration1[1])
-    print(ar.takeMachineInput(ditVal))
-else:
-    transformed = T.textToOutput(toSend)
-    print(transformed)
-    aw.writeToArduino(transformed)
+
+    print(ar.takeMachineInput(ditVal, arduino))
+else:           # this computer is the sender
+    string = input('Enter input string in English: ')
+    l = T.textToOutput(string)  # get the string of sums to work with
+
+    # for char in l:
+    #     if char != '1':
+    #         print(char, end = '')
+
+    aw.writeToArduino(l, arduino)
